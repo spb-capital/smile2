c67fefada88e564fa4264b1ccc6559bf3479038e
samisahn <sami.sahnoune@mail.mcgill.ca>
2018-05-12 15:30:09 -0500

    switched precision to knob components, automatic updates upon param change

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ info = {}
 app.layout = html.Div(id = 'container', children = [
 # LEFT SIDE
     html.Div([
-        html.H2("Dash Instrumentation"),
+        html.H2("Dash DAQ: Function Generator & Oscilloscope Control Panel", style = {'marginLeft': '40px'}),
     ], className='banner'),
     html.Div('', id = 'settings1'),
     html.Div('', id = 'settings2'),
@@ -38,31 +38,55 @@ app.layout = html.Div(id = 'container', children = [
                     html.H3("POWER")
                 ], className='Title'),
                 html.Div([
-                    html.Div([daq.PowerButton(id = 'fnct_power', on = 'true', label = "FNCT", labelPosition = 'bottom')], className = 'six columns', style = {'margin-bottom': '15px'}),
-                    html.Div([daq.PowerButton(id = 'osc_power', on = 'true', label = "OSC", labelPosition = 'bottom')], className = 'six columns', style = {'margin-bottom': '15px'}),
+                    html.Div([daq.PowerButton(id = 'fnct_power', on = 'true', label = "Function Generator", labelPosition = 'bottom')], className = 'six columns', style = {'margin-bottom': '15px'}),
+                    html.Div([daq.PowerButton(id = 'osc_power', on = 'true', label = "Oscilloscope", labelPosition = 'bottom')], className = 'six columns', style = {'margin-bottom': '15px'}),
                 ], style = {'margin': '15px 0'})
                 ], className='row power-settings-tab'),
         html.Div([
                 html.Div([
                     html.H3("FUNCTION")
                 ], className = 'Title'),
-                html.Div([
-                    "Frequency (Hz)",
-                    # daq.PrecisionInput(precision = 2, value = 10E5, style = {'float': 'right'}),
-                    # dcc.Input(id='frequency_input', type='text', value = 10E5, style = {'float': 'right', 'textAlign': 'center'}),
-                    daq.PrecisionInput(id = 'frequency_input', precision = 4, value = 1E6, min = 1E3, max = 2.5E6, size = 100, style = {'float': 'right'}),
-                ], style = {'margin': '15px 0'}),
-                html.Div([
-                    "Amplitude (μm)",
-                    daq.PrecisionInput(id = 'amplitude_input', precision = 4, value = 1, min = 0.5, max = 10, size = 100, style = {'float': 'right'}),
-                ], style = {'margin': '20px 0'}),
-                html.Div([
-                    "Offset (μm)",
-                    daq.PrecisionInput(id = 'offset_input', precision = 4, value = 0, min = 0.5, max = 10, size = 100, style = {'float': 'right'}),
-                ], style = {'margin': '20px 0'}),
+                html.Div([daq.Knob(
+                value = 1E6,
+                id = "frequency_input",
+                label = "Frequency (Hz)",
+                labelPosition = "bottom",
+                  size=75,
+                  color='rgb(68, 126, 255)',
+                  # add custom ticks
+                  scale = {'interval': 1E5, 'custom': {'2.5E6': {'label': '2.5E6', 'style': {'color': 'red'}}}},
+                  max = 2.5E6,
+                  min = 1E5
+                ),
+                daq.Knob(
+                value = 1,
+                id = "amplitude_input",
+                label = "Amplitude (μV)",
+                labelPosition = "bottom",
+                  size=75,
+                  scale = {'labelInterval': 10},
+                  color='rgb(68, 126, 255)',
+                  max = 10,
+                ),
+                daq.Knob(
+                  value = 0,
+                  id = "offset_input",
+                  label = "Offset (μV)",
+                  labelPosition = "bottom",
+                  size=75,
+                  scale = {'labelInterval': 10},
+                  color='rgb(68, 126, 255)',
+                  max = 10,
+                )],
+                style = {'margin': '0 auto',
+                    'display': 'flex',
+                    'width': '80%',
+                    'alignItems': 'center',
+                    'justifyContent': 'center'
+                }),
                 dcc.RadioItems(
                     id = 'function_type',
-                    options=[
+                    options = [
                         {'label': 'Sine', 'value': 'SIN'},
                         {'label': 'Square', 'value': 'SQUARE'},
                         {'label': 'Ramp', 'value': 'RAMP'},
@@ -71,15 +95,15 @@ app.layout = html.Div(id = 'container', children = [
                     value='SIN',
                     labelStyle={'display': 'inline-block'},
                     style = {'margin': '0 auto',
-                    'display': 'flex',
-                    'width': '80%',
-                    'alignItems': 'center',
-                    'justifyContent': 'space-between'}
+                        'display': 'flex',
+                        'width': '80%',
+                        'alignItems': 'center',
+                        'justifyContent': 'space-between'}
                 )
             ], className='row power-settings-tab'),
         html.Hr(),
-        html.Button('Update', id='setting_submit', type = 'submit'),
-    ], className = 'three columns left-tab'),
+        # html.Button('Update', id='setting_submit', type = 'submit'),
+    ], className = 'four columns left-tab'),
 
 
 # RIGHT SIDE
@@ -123,7 +147,7 @@ app.layout = html.Div(id = 'container', children = [
                 'displayModeBar': False
             }
         )
-        ], className = 'eight columns graph'),
+        ], className = 'seven columns graph'),
 ])
 
 # @app.callback(Output('tabs', 'tabs'),
@@ -143,13 +167,14 @@ def update_details(figure, value):
         return "NO DATA"
 
 @app.callback(Output('oscope', 'figure'),
-            [Input('setting_submit', 'n_clicks'),
-             Input('tabs', 'value')],
-            [State('frequency_input', 'value'),
-            State('function_type', 'value'),
-            State('amplitude_input', 'value'),
-            State('offset_input', 'value')])
-def update_output(n_clicks, value, frequency, wave, amplitude, offset):
+            [
+            # Input('setting_submit', 'n_clicks'),
+            Input('tabs', 'value'),
+            Input('frequency_input', 'value'),
+            Input('function_type', 'value'),
+            Input('amplitude_input', 'value'),
+            Input('offset_input', 'value')])
+def update_output(value, frequency, wave, amplitude, offset):
     global tab
     time = np.linspace(-0.000045, 0.000045, 1e3)
     zero = dict(
@@ -168,45 +193,45 @@ def update_output(n_clicks, value, frequency, wave, amplitude, offset):
             )
         )
 
-    if n_clicks is not None:
-        if tab is not value:
-            if('' + str(value) in runs):
-                tab = value
-                return runs['' + str(value)][0]
-            else:
-                tab = value
-                return zero
-
+    # if n_clicks is not None:
+    if tab is not value:
+        if('' + str(value) in runs):
+            tab = value
+            return runs['' + str(value)][0]
         else:
-            if(wave == 'SIN'):
-                y = [float(offset) + (float(amplitude) * np.sin(np.radians(2.0 * np.pi * float(frequency) * t))) for t in time]
+            tab = value
+            return zero
 
-            elif(wave == 'SQUARE'):
-                y = [float(offset) + float(amplitude) *(signal.square(2.0 * np.pi * float(frequency)/10 * t)) for t in time]
+    else:
+        if(wave == 'SIN'):
+            y = [float(offset) + (float(amplitude) * np.sin(np.radians(2.0 * np.pi * float(frequency) * t))) for t in time]
 
-            elif(wave == 'RAMP'):
-                y = float(amplitude) * (np.abs(signal.sawtooth(2 * np.pi * float(frequency)/10 * time)))
-                y = float(offset) + 2*y - float(amplitude)
+        elif(wave == 'SQUARE'):
+            y = [float(offset) + float(amplitude) *(signal.square(2.0 * np.pi * float(frequency)/10 * t)) for t in time]
 
-            figure = dict(
-                    data = [dict(x = time, y = y, marker = {'color': 'rgb(68, 126, 255)'})],
-                    layout =  go.Layout(
-                        xaxis={'type': 'linear', 'title': 's', 'titlefont': dict(
-                            family='Dosis',
-                            size=15,
-                        )},
-                        yaxis={'title': 'Voltage (V)','titlefont': dict(
-                            family='Dosis',
-                            size=15,
-                        )},
-                        margin={'l': 40, 'b': 40, 't': 0, 'r': 50},
-                        plot_bgcolor = 'rgb(240, 240, 240)',
-                    )
+        elif(wave == 'RAMP'):
+            y = float(amplitude) * (np.abs(signal.sawtooth(2 * np.pi * float(frequency)/10 * time)))
+            y = float(offset) + 2*y - float(amplitude)
+
+        figure = dict(
+                data = [dict(x = time, y = y, marker = {'color': 'rgb(68, 126, 255)'})],
+                layout =  go.Layout(
+                    xaxis={'type': 'linear', 'title': 's', 'titlefont': dict(
+                        family='Dosis',
+                        size=15,
+                    )},
+                    yaxis={'title': 'Voltage (V)','titlefont': dict(
+                        family='Dosis',
+                        size=15,
+                    )},
+                    margin={'l': 40, 'b': 40, 't': 0, 'r': 50},
+                    plot_bgcolor = 'rgb(240, 240, 240)',
                 )
+            )
 
-            runs['' + str(value)] = figure, "WAVE: " + str(wave) + "      " + str(frequency) +  " Hz" + "      " + str(amplitude) + " μm"
+        runs['' + str(value)] = figure, "WAVE: " + str(wave) + "      " + str(frequency) +  " Hz" + "      " + str(amplitude) + " μV"
 
-            return figure
+        return figure
 
 
 @app.callback(
