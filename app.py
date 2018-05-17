64cea4050b8ed1c8d301e4cb97595f3f6266d288
samisahn <sami.sahnoune@mail.mcgill.ca>
2018-05-17 17:45:54 -0500

    revised to pep8 via pyflakes

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ runs = {}
 app.layout = html.Div(id='container', children=[
     # LEFT SIDE
     html.Div([
-        html.H2("Dash DAQ: Function Generator & Oscilloscope Control Panel", style={'marginLeft': '40px'}),
+        html.H2("Dash DAQ: Function Generator & Oscilloscope Control Panel",
+                style={'marginLeft': '40px'}),
     ], className='banner', id="header"),
 
     html.Div([
@@ -36,14 +37,34 @@ app.layout = html.Div(id='container', children=[
                 html.H3("POWER", id="power_title")
             ], className='Title'),
             html.Div([
-                html.Div([daq.PowerButton(id='fnct_power', on='true', label="Function Generator", labelPosition='bottom', color="#447EFF")], className='six columns', style={'margin-bottom': '15px'}),
-                html.Div([daq.PowerButton(id='osc_power', on='true', label="Oscilloscope", labelPosition='bottom', color="#447EFF")], className='six columns', style={'margin-bottom': '15px'}),
+                html.Div(
+                    [
+                        daq.PowerButton(
+                            id='fnct_power',
+                            on='true',
+                            label="Function Generator",
+                            labelPosition='bottom',
+                            color="#447EFF")
+                    ],
+                    className='six columns',
+                    style={'margin-bottom': '15px'}),
+                html.Div(
+                    [
+                        daq.PowerButton(
+                            id='osc_power',
+                            on='true',
+                            label="Oscilloscope",
+                            labelPosition='bottom',
+                            color="#447EFF")
+                    ],
+                    className='six columns',
+                    style={'margin-bottom': '15px'}),
             ], style={'margin': '15px 0'})
         ], className='row power-settings-tab'),
         html.Div([
-            html.Div([
-                html.H3("FUNCTION", id="function_title")
-            ], className='Title'),
+            html.Div(
+                [html.H3("FUNCTION", id="function_title")],
+                className='Title'),
             html.Div([
                 daq.Knob(
                     value=1E6,
@@ -76,23 +97,44 @@ app.layout = html.Div(id='container', children=[
                     color="#447EFF",
                     max=10,
                 )],
-                     style={'margin': '0 auto',
-                            'display': 'flex',
-                            'width': '80%',
-                            'alignItems': 'center',
-                            'justifyContent': 'center'
-                           }),
-
+                     style={
+                         'margin': '0 auto',
+                         'display': 'flex',
+                         'width': '80%',
+                         'alignItems': 'center',
+                         'justifyContent': 'center'
+                         }),
             html.Div([
-                daq.LEDDisplay(id='freq', size=10, value=1E6, label="Frequency (Hz)", labelPosition="bottom", color="#447EFF", style={'textAlign': 'center'}),
-                daq.LEDDisplay(id='ampl', size=10, value=1, label="Amplitude (mV)", labelPosition="bottom", color="#447EFF", style={'textAlign': 'center'}),
-                daq.LEDDisplay(id='off', size=10, value=10, label="Offset (mV)", labelPosition="bottom", color="#447EFF", style={'textAlign': 'center'}),
-            ], style={'margin': '0 auto',
-                      'display': 'flex',
-                      'width': '80%',
-                      'alignItems': 'center',
-                      'justifyContent': 'space-between'
-                     }),
+                daq.LEDDisplay(
+                    id='freq',
+                    size=10, value=1E6,
+                    label="Frequency (Hz)",
+                    labelPosition="bottom",
+                    color="#447EFF",
+                    style={'textAlign': 'center'}),
+                daq.LEDDisplay(
+                    id='ampl',
+                    size=10,
+                    value=1,
+                    label="Amplitude (mV)",
+                    labelPosition="bottom",
+                    color="#447EFF",
+                    style={'textAlign': 'center'}),
+                daq.LEDDisplay(
+                    id='off',
+                    size=10,
+                    value=10,
+                    label="Offset (mV)",
+                    labelPosition="bottom",
+                    color="#447EFF",
+                    style={'textAlign': 'center'}),
+            ], style={
+                'margin': '0 auto',
+                'display': 'flex',
+                'width': '80%',
+                'alignItems': 'center',
+                'justifyContent': 'space-between'
+                }),
 
             dcc.RadioItems(
                 id='function_type',
@@ -123,7 +165,7 @@ app.layout = html.Div(id='container', children=[
 
     # RIGHT SIDE
     html.Div([
-        html.Div([html.H3("GRAPH", id="graph_title"),], className='Title'),
+        html.Div([html.H3("GRAPH", id="graph_title")], className='Title'),
         dcc.Tabs(
             tabs=tabs,
             value=1,
@@ -132,15 +174,31 @@ app.layout = html.Div(id='container', children=[
         ),
 
         html.Div([
-            html.Div([html.Div("NO DATA", id="details", style={'textAlign': 'center', 'fontSize': '10px', 'padding': '0px 5px', 'lineHeight': '20px', 'border': '2px solid rgb(240, 240, 240)'})], className="four columns"),
+            html.Div([
+                html.Div("NO DATA",
+                         id="details",
+                         style={
+                             'textAlign': 'center',
+                             'fontSize': '10px', 'padding': '0px 5px',
+                             'lineHeight': '20px',
+                             'border': '2px solid rgb(240, 240, 240)'
+                         })
+                ], className="four columns"),
             html.Div(id="curr_info"),
-            html.Button('+', id='new_tab', type='submit', style={'height': '20px', 'width': '20px', 'padding': '0px', 'lineHeight': '10px', 'float': 'right'}),
+            html.Button('+',
+                        id='new_tab',
+                        type='submit',
+                        style={'height': '20px', 'width': '20px',
+                               'padding': '0px', 'lineHeight': '10px',
+                               'float': 'right'}),
         ], className='row graph-info', style={'margin': '15px'}),
         html.Hr(),
         dcc.Graph(
             id='oscope',
             figure=dict(
-                data=[dict(x=np.linspace(-0.000045, 0.000045, 1e3), y=[0] * len(np.linspace(-0.000045, 0.000045, 1e3)), marker={'color': 'rgb(68, 126, 255)'})],
+                data=[dict(x=np.linspace(-0.000045, 0.000045, 1e3),
+                           y=[0] * len(np.linspace(-0.000045, 0.000045, 1e3)),
+                           marker={'color': 'rgb(68, 126, 255)'})],
                 layout=go.Layout(
                     xaxis={'type': 'linear', 'title': 's', 'titlefont': dict(
                         family='Dosis',
@@ -155,100 +213,112 @@ app.layout = html.Div(id='container', children=[
                 )
             ),
             config={'displayModeBar': True,
-                    'modeBarButtonsToRemove': ['pan2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian']}
+                    'modeBarButtonsToRemove': ['pan2d',
+                                               'zoomIn2d',
+                                               'zoomOut2d',
+                                               'autoScale2d',
+                                               'hoverClosestCartesian',
+                                               'hoverCompareCartesian']}
         )
     ], className='seven columns graph'),
 ])
 
-@app.callback(
-    Output('freq', 'color'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('freq', 'color'),
+              [Input('color-picker', 'value')])
 def frequency_display(color):
     return color['hex']
 
-@app.callback(
-    Output('ampl', 'color'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('ampl', 'color'),
+              [Input('color-picker', 'value')])
 def amplitude_display(color):
     return color['hex']
 
-@app.callback(
-    Output('off', 'color'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('off', 'color'),
+              [Input('color-picker', 'value')])
 def offset_display(color):
     return color['hex']
 
-@app.callback(
-    Output('tabs', 'style'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('tabs', 'style'),
+              [Input('color-picker', 'value')])
 def tabs_background(color):
     return {'backgroundColor': color['hex']}
 
-@app.callback(
-    Output('power_title', 'style'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('power_title', 'style'),
+              [Input('color-picker', 'value')])
 def power_title(color):
     return {'color': color['hex']}
 
-@app.callback(
-    Output('function_title', 'style'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('function_title', 'style'),
+              [Input('color-picker', 'value')])
 def function_title(color):
     return {'color': color['hex']}
 
-@app.callback(
-    Output('graph_title', 'style'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('graph_title', 'style'),
+              [Input('color-picker', 'value')])
 def graph_title(color):
     return {'color': color['hex']}
 
-@app.callback(
-    Output('fnct_power', 'color'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('fnct_power', 'color'),
+              [Input('color-picker', 'value')])
 def fnct_style(color):
     return color['hex']
 
-@app.callback(
-    Output('osc_power', 'color'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('osc_power', 'color'),
+              [Input('color-picker', 'value')])
 def osc_style(color):
     return color['hex']
 
-@app.callback(
-    Output('header', 'style'),
-    [Input('color-picker', 'value')])
+
+@app.callback(Output('header', 'style'),
+              [Input('color-picker', 'value')])
 def update_header(color):
     return {'backgroundColor': color['hex']}
+
 
 @app.callback(Output('frequency_input', 'color'),
               [Input('color-picker', 'value')])
 def set_freq_color(color):
     return color['hex']
 
+
 @app.callback(Output('amplitude_input', 'color'),
               [Input('color-picker', 'value')])
 def set_amplitude_color(color):
     return color['hex']
+
 
 @app.callback(Output('offset_input', 'color'),
               [Input('color-picker', 'value')])
 def set_offset_color(color):
     return color['hex']
 
+
 @app.callback(Output('ampl', 'value'),
               [Input('amplitude_input', 'value')],)
 def update_amplPI(value):
     return value
+
 
 @app.callback(Output('freq', 'value'),
               [Input('frequency_input', 'value')],)
 def update_freqPI(value):
     return value
 
+
 @app.callback(Output('off', 'value'),
               [Input('offset_input', 'value')])
 def update_offPI(value):
     return value
+
 
 @app.callback(Output('details', 'children'),
               [Input('oscope', 'figure'),
@@ -258,6 +328,7 @@ def update_details(_, value):
         return runs['' + str(value)][1]
     return "NO DATA"
 
+
 @app.callback(Output('oscope', 'figure'),
               [Input('tabs', 'value'),
                Input('frequency_input', 'value'),
@@ -266,7 +337,7 @@ def update_details(_, value):
                Input('offset_input', 'value'),
                Input('osc_power', 'on'),
                Input('fnct_power', 'on')])
-def update_output(value, frequency, wave, amplitude, offset, osc_on, fnct_power):
+def update_output(value, frequency, wave, amplitude, offset, osc_on, fnct_on):
     global tab
 
     time = np.linspace(-0.000045, 0.000045, 1e3)
@@ -303,7 +374,7 @@ def update_output(value, frequency, wave, amplitude, offset, osc_on, fnct_power)
             )
         )
 
-    if not fnct_power:
+    if not fnct_on:
         return zero
 
     if tab is not value:
@@ -315,13 +386,20 @@ def update_output(value, frequency, wave, amplitude, offset, osc_on, fnct_power)
 
     else:
         if wave == 'SIN':
-            y = [float(offset) + (float(amplitude) * np.sin(np.radians(2.0 * np.pi * float(frequency) * t))) for t in time]
+            y = [float(offset) +
+                 (float(amplitude) *
+                  np.sin(np.radians(2.0 * np.pi * float(frequency) * t)))
+                 for t in time]
 
         elif wave == 'SQUARE':
-            y = [float(offset) + float(amplitude) *(signal.square(2.0 * np.pi * float(frequency)/10 * t)) for t in time]
+            y = [float(offset) +
+                 float(amplitude) *
+                 (signal.square(2.0 * np.pi * float(frequency)/10 * t))
+                 for t in time]
 
         elif wave == 'RAMP':
-            y = float(amplitude) * (np.abs(signal.sawtooth(2 * np.pi * float(frequency)/10 * time)))
+            y = float(amplitude) * \
+                (np.abs(signal.sawtooth(2*np.pi * float(frequency)/10 * time)))
             y = float(offset) + 2*y - float(amplitude)
 
         figure = dict(
@@ -340,7 +418,8 @@ def update_output(value, frequency, wave, amplitude, offset, osc_on, fnct_power)
             )
         )
 
-        runs['' + str(value)] = figure, str(wave) + "\t" + str(frequency) +  " Hz" + "\t" + str(amplitude) + "mV"
+        runs['' + str(value)] = figure, str(wave) + "\t" + str(frequency) +  \
+            " Hz" + "\t" + str(amplitude) + "mV"
 
         return figure
 
@@ -351,22 +430,27 @@ def update_output(value, frequency, wave, amplitude, offset, osc_on, fnct_power)
 def fnct_indication(on):
     return on
 
+
 @app.callback(
     Output('osc_indicator', 'value'),
     [Input('osc_power', 'on')])
 def osc_indication(on):
     return on
 
+
 @app.callback(Output('tabs', 'tabs'),
               [Input('new_tab', 'n_clicks')])
 def new_tabs(n_clicks):
     if n_clicks is not None:
-        tabs.append({'label': 'Run #' + str(tabs[-1]['value'] + 1), 'value': int(tabs[-1]['value']) + 1})
+        tabs.append({'label': 'Run #' + str(tabs[-1]['value'] + 1),
+                     'value': int(tabs[-1]['value']) + 1})
         return tabs
     return tabs
 
+
 external_css = ["https://codepen.io/chriddyp/pen/bWLwgP.css",
-                "https://cdn.rawgit.com/samisahn/dash-app-stylesheets/09e3a2ee/dash-tektronix-350.css",
+                "https://cdn.rawgit.com/samisahn/dash-app-stylesheets/" +
+                "09e3a2ee/dash-tektronix-350.css",
                 "https://fonts.googleapis.com/css?family=Dosis"]
 
 for css in external_css:
