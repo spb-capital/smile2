e3c581d85037a78a331369e2c1ec74b067359434
ycaokris <yi.cao2@mail.mcgill.ca>
2019-02-20 12:32:20 -0500

    Remove config serve locally

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ import osc_tds350 as osc
 
 app = dash.Dash()
 
-app.scripts.config.serve_locally = True
 app.config['suppress_callback_exceptions'] = True
 
 server = app.server
--- app_mock.py
+++ app_mock.py
@@ -14,7 +14,6 @@ import os
 
 app = dash.Dash()
 
-app.scripts.config.serve_locally = True
 app.config['suppress_callback_exceptions'] = True
 
 server = app.server
